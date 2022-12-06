64276735c88920995e3d36f07d78100e415422b0
Bryan Ramos Vargas <matematica_bryan@hotmail.com>
2022-12-06 13:30:22 -0500

    nav style

--- src/Components/Logout/Logout.jsx
+++ src/Components/Logout/Logout.jsx
@@ -1,10 +1,14 @@
 import React from "react";
+import { UilSignout } from "@iconscout/react-unicons";
+
 import { useAuth0 } from "@auth0/auth0-react";
-import s from "./Logout.module.css"
+import s from "./Logout.module.css";
 
 export default function Logout() {
   const { logout } = useAuth0();
   return (
-      <button onClick={() => logout()} className={s.logout}>Logout</button>
+    <button onClick={() => logout()} className={s.logout}>
+      <UilSignout /> Log Out
+    </button>
   );
 }
