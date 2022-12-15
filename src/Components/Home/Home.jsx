6d7abae3f42f96bf46dc9220c2bade3cf9a40b8e
Issmaa <isma_h21@outlook.com>
2022-12-14 19:25:50 -0600

    modal inicio

--- src/Components/Home/Home.jsx
+++ src/Components/Home/Home.jsx
@@ -1,10 +1,14 @@
-import React from "react";
+import React, { useEffect } from "react";
 import UpdateProfile from "../UpdateProfile/UpdateProfile";
 // import axios from "axios";
 // import { useAuth0 } from "@auth0/auth0-react";
 import s from "./Home.module.css";
+import Modal from '../Modal/Modal';
+import { useState } from "react";
+
 
 export default function Home() {
+  const [openModal, setOpenModal] = useState(false);
   // const { getAccessTokenSilently } = useAuth0();
 
   // const handleOnClick = async () => {
@@ -26,6 +30,11 @@ export default function Home() {
   return (
     <section className={s.container}>
       <h1>Match App!!</h1>
+      <h2>Dejanos conocerte</h2>
+      <p>Para conocer tus intereses y poder conectar con otros Henry's que compartan tus gustos porfavor completa el formulario ingresado al boton.</p>
+      <button 
+      onClick={() => {setOpenModal(true)}}> Open </button>
+      {openModal && <Modal closeModal={setOpenModal}/>}
       {/* <button onClick={handleOnClick}>API</button> */}
       {/* <UpdateProfile /> */}
     </section>
